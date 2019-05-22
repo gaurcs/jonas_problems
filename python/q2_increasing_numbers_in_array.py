c8a678481f246bf5176acb81d5f0abfe284ebff4
Vasu Gaur <vagaur@cisco.com>
2019-05-22 15:44:30 -0700

    Add q2

---
+++ python/q2_increasing_numbers_in_array.py
@@ -0,0 +1,9 @@
+''' Problem Statement :
+
+1) Given an array of numbers, return whether the array has three numbers that are increasing, that is index i,j,k such that i< j < k and a[i] < a[j] < a[k].
+Example:
+	Given: [4,2,5,1,7]
+	Output: True
+
+'''
+
