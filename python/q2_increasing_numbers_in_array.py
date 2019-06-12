5f470d682a275c0129088ddf13b22600d6f40a8d
Vasu Gaur <vagaur@cisco.com>
2019-06-11 22:36:13 -0700

    Add q2

--- python/q2_increasing_numbers_in_array.py
+++ python/q2_increasing_numbers_in_array.py
@@ -13,10 +13,38 @@ import unittest
 
 def increasing_order(array):
 
+	# if 'Number of elements in array are less than 3'
 	if len(array) < 3:
-		raise ValueError('Number of elements in array are less than 3')
+		return False
 
-	
+	# first two pointers to max value
+	first = float('inf')
+	second = float('inf')
+	for i in range(0,len(array)):
+		if array[i] <= first:
+			first = array[i]
+		elif array[i] <= second:
+			second = array[i]
+		else:
+			return True
+
+	return False
+
+class Test(unittest.TestCase):
+	data = [
+	([], False),
+	([1,1], False),
+	([1,1,1], False),
+	([1,2,3,4,5,6], True),
+	([6,5,4,3,2,1], False),
+	([6,5,4,3,2,1,0,2,4], True)]
+
+	def test_increasing_order(self):
+		for [test_list, expected] in self.data:
+			actual = increasing_order(test_list)
+			self.assertEqual(actual, expected)
+
+
 if __name__ == '__main__':
 	unittest.main()
 
