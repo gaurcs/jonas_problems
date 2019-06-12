2425b0e12d642f6a4b9b83d4e7d9058bf6aa0bec
Vasu Gaur <vagaur@cisco.com>
2019-06-11 22:09:48 -0700

    re-arrange files in order

--- python/q2_increasing_numbers_in_array.py
+++ python/q2_increasing_numbers_in_array.py
@@ -1,9 +1,24 @@
 ''' Problem Statement :
 
-1) Given an array of numbers, return whether the array has three numbers that are increasing, that is index i,j,k such that i< j < k and a[i] < a[j] < a[k].
+1) Given an array of numbers, return whether the array has three numbers that
+ are increasing, that is index i,j,k such that i<j<k and a[i]<a[j]<a[k].
+
 Example:
 	Given: [4,2,5,1,7]
 	Output: True
 
 '''
+
+import unittest
+
+def increasing_order(array):
+
+	if len(array) < 3:
+		raise ValueError('Number of elements in array are less than 3')
+
+	
+if __name__ == '__main__':
+	unittest.main()
+
+
 
